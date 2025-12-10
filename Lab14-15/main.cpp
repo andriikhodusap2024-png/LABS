bda377c468103595495f3480366f2fa2ab344614
Andronius <andrii.khodus.ap.2024@lpnu.ua>
2025-12-10 14:30:22 +0200

    Lab14-15

--- Lab14-15/main.cpp
+++ Lab14-15/main.cpp
@@ -10,13 +10,15 @@ int main() {
 
     DoublyLinkedList list;
 
+    cout << "Створення та заповнення списку...\n";
     list.pushBack(Transformer(2.5, 220, 500));
     list.pushBack(Transformer(4.0, 380, 2000));
     list.pushFront(Transformer(1.8, 110, 300));
 
+    cout << "\nПочатковий список (прямий вивід):\n";
     list.displayForward();
-    cout << endl;
 
+    cout << "\nВставка елемента після коефіцієнта 2.5:\n";
     list.insertAfter(2.5, Transformer(3.3, 220, 750));
     list.displayForward();
 
@@ -24,7 +26,15 @@ int main() {
     list.removeByRatio(4.0);
     list.displayForward();
 
-    cout << "\nЗворотний вивід:\n";
+    cout << "\nВидалення елемента з початку:\n";
+    list.removeFront();
+    list.displayForward();
+
+    cout << "\nВидалення елемента з кінця:\n";
+    list.removeBack();
+    list.displayForward();
+
+    cout << "\nЗворотний вивід списку:\n";
     list.displayBackward();
 
     return 0;
